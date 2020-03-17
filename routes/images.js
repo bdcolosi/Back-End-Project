77d4106d9aa6081374f834679ad6dcea87775dd7
bdcolosi <bdcolosi27@gmail.com>
2020-03-17 15:41:46 -0400

    finished version

--- routes/images.js
+++ routes/images.js
@@ -4,10 +4,10 @@ imageModel = require('../models/imageModel'),
 commentsModel = require('../models/commentsModel');
 
 
+
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   const resultData = await imageModel.getAllPictures();
-
   res.render('template', {
     locals: {
       title: 'Film Data',
@@ -20,42 +20,54 @@ router.get('/', async function(req, res, next) {
   })
 });
 
+
 router.get("/:picture_id", async function(req, res, next) {
   const { picture_id } = req.params;
-  console.log(picture_id);
   const user_id = req.session.user_id;
-  const data = await imageModel.getPicturesById(picture_id);
+  const resultData = await imageModel.getPicturesById(picture_id);
   const profileData = await imageModel.getProfilePicture(user_id);
-  const commentData = await commentsModel.getCommentsByImageId(picture_id);
+  const commentData = await commentsModel.getCommentsByImageId(picture_id)
+  if (commentData == undefined) {
+    res.render("template", {
+      locals: {
+        title: 'Film Data',
+        user_id: user_id,
+        resultData: resultdata,
+        profileData: profileData,
+        name: req.session.name,
+        is_logged_in: req.session.is_logged_in
+      },
+      partials: {
+        partial: "partial-single-img"
+      }
+    })
+  } else {
+    res.render("template", {
+      locals: {
+        title: 'Film Data',
+        user_id: user_id,
+        resultData: resultData,
+        profileData: profileData,
+        commentData:commentData,
+        name: req.session.name,
+        is_logged_in: req.session.is_logged_in,
+      },
+      partials: {
+        partial:"partial-single-img"
+      }
+    })
 
-  
+  }});
 
-  res.render("template", {
-    locals: {
-      title: 'Film Data',
-      user_id: user_id,
-      data: data,
-      profileData: profileData,
-      commentData: commentData,
-      name: req.session.name,
-      is_logged_in: req.session.is_logged_in
-    },
-    partials: {
-      partial: "partial-single-img"
-    }
-  });
-});
 
 
 router.post("/comment", async (req, res) => {
   const { picture_id, comment } = req.body;
   const user_id = req.session.user_id;
-  const postData = new commentsModel(null, user_id, picture_id, comment, null)
+  const postData = new commentsModel(null, user_id, picture_id, comment, null);
     postData.addComment().then(() => {
       res.redirect('/')
     });
 });
 
 module.exports = router;
-
-
--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,14 @@ likesModel = require('../models/likesModel');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const user_id = req.session.user_id;
   const resultData = await imageModel.getAllPictures();
   const user_id = req.session.user_id; 
   const profileData = await imageModel.getProfilePicture(user_id);
-  console.log(profileData[0]);
-
   console.log(req.session.name);
   res.render('template', {
     locals: {
       title: 'Film Data',
-      profileData: profileData,
-      user_id: req.session.user_id,
+      user: req.session.user_id,
       resultData: resultData,
       profileData: profileData,
       is_logged_in: req.session.is_logged_in,
